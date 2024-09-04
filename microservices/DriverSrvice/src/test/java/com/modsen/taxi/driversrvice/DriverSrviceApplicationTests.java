912fc9b39a2d77a3c3d142b91a94229ff2264caa
Matvey Voropay <vorofpie@gmail.com>
2024-09-04 18:11:13 +0300

    Create project

---
+++ microservices/DriverSrvice/src/test/java/com/modsen/taxi/driversrvice/DriverSrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.taxi.driversrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverSrviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
