589cd109317b330a75d016096e1351df59aed46e
Matvey Voropay <vorofpie@gmail.com>
2024-09-07 13:49:36 +0300

    Add persistence layer for trip service

---
+++ microservices/TripService/src/main/java/com/modsen/taxi/tripservice/domain/Trip.java
@@ -0,0 +1,33 @@
+package com.modsen.taxi.tripservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "trips")
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long driverId;
+    private Long passengerId;
+
+    private String originAddress;
+    private String destinationAddress;
+
+    @Enumerated(EnumType.STRING)
+    private TripStatus status;
+
+    private LocalDateTime orderDateTime;
+
+    private BigDecimal price;
+}
